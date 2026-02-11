3adf869fbf54e04bccba6a457128fcfb5bdafef0
BigLineDev0 <alioudiallo.py@gmail.com>
2026-02-11 08:55:01 +0000

    Ajout fonctionnalité authentification et gestion des roles

--- schema.sql
+++ schema.sql
@@ -6,3 +6,7 @@ CREATE TABLE utilisateurs (
     password VARCHAR(255) NOT NULL,
     role ENUM('admin', 'user') DEFAULT 'user'
 );
+
+INSERT INTO utilisateurs(prenom, nom, email, password, role) VALUES
+("Aliou", "DIALLO", "aliou@gmail.com", "Passer@123", "admin"),
+("Mame Saye", "LOM", "mamesaye@gmail.com", "User@123", "user");
